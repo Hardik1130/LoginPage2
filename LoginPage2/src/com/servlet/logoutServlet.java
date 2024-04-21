e19227be5c96ac319e85569225ef12979193db6f
hardikgupta <hardikgupta176@gmail.com>
2024-04-21 23:14:26 +0530

    Initial commit

---
+++ LoginPage2/src/com/servlet/logoutServlet.java
@@ -0,0 +1,29 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class logoutServlet
+ */
+@WebServlet("/logoutServlet")
+public class logoutServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+		HttpSession session = request.getSession();
+		session.removeAttribute("user-ob");
+		
+		HttpSession session2 = request.getSession();
+		session.setAttribute("logout-msg","Logout Successfully");
+		
+		response.sendRedirect("login.jsp");
+		
+	}
+}
